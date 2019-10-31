6d82460e63d2ddcb73e91e50840b939272c9793f
pvasquez <alexanderpavelv32@gmail.com>
2019-10-30 22:45:46 -0600

    Sentencia while terminada
    Se finalizo la sentencia while, se descubrieron ciertos bugs en las operaciones relacionales, falta arreglar eso.

--- src/Expresiones/Operacion.java
+++ src/Expresiones/Operacion.java
@@ -180,7 +180,16 @@ public class Operacion implements Expresion {
                 codigo += "^," + op1Actual + "," + op2Actual + "," + tabla.getTemporal() + "\n";
                 break;
             case MENOR_QUE:
-                codigo += "jl," + op1Actual + "," + op2Actual + "," + tabla.getEtiqueta() + "\n";
+                String temp1 = tabla.getTemporal();
+                String label1 = tabla.getEtiqueta();
+                String label2 = tabla.getEtiqueta();
+                codigo += "jl," + op1Actual + "," + op2Actual + "," + label1 + "\n";
+                codigo += "=,0,," + temp1 + "\n";
+                codigo += "jmp,,," + label2 + "\n";
+                codigo += label1 + ":\n";
+                codigo += "=,1,," + temp1 + "\n";
+                codigo += label2 + ":\n";
+                codigo += "=," + temp1 + ",," + tabla.getTemporal() + "\n";
                 break;
             case MENOR_IGUAL:
                 codigo += "jle," + op1Actual + "," + op2Actual + "," + tabla.getEtiqueta() + "\n";
---
+++ src/Instrucciones/While.java
@@ -0,0 +1,68 @@
+package Instrucciones;
+
+import Excepciones.Excepcion;
+import Interfaces.AST;
+import Interfaces.Expresion;
+import Interfaces.Instruccion;
+import TablaSimbolos.Tabla;
+import TablaSimbolos.Tree;
+
+import java.util.ArrayList;
+
+public class While implements Instruccion {
+    private Expresion condicion;
+    private ArrayList<AST> instrucciones;
+    private int fila;
+    private int columna;
+
+    public While(Expresion condicion, ArrayList<AST> instrucciones, int fila, int columna) {
+        this.condicion = condicion;
+        this.instrucciones = instrucciones;
+        this.fila = fila;
+        this.columna = columna;
+    }
+
+    @Override
+    public Object ejecutar(Tabla tabla, Tree arbol) {
+        Object result = this.condicion.getTipo(tabla, arbol);
+        if (result instanceof Excepcion) {
+            return result;
+        }
+
+        for (int i = 0; i < this.instrucciones.size(); i++) {
+            AST ast = this.instrucciones.get(i);
+            if (ast instanceof Instruccion) {
+                result = ((Instruccion) ast).ejecutar(tabla, arbol);
+            } else {
+                result = ((Expresion) ast).getValor(tabla, arbol);
+            }
+            if (result instanceof Excepcion) {
+                return result;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int getEspacios(int espacios) {
+        return 0;
+    }
+
+    @Override
+    public Object get4D(Tabla tabla, Tree arbol) {
+        String codigo = "";
+        String temp1 = tabla.getTemporal();
+        String label1 = tabla.getEtiqueta();
+        String label2 = tabla.getEtiqueta();
+        codigo += label2 + ":\n";
+        codigo += this.condicion.get4D(tabla, arbol);
+        codigo += "=," + tabla.getTemporalActual() + ",," + temp1 + "\n";
+        codigo += "je," + temp1 + ",0," + label1 + "\n";
+        for (int i = 0; i < this.instrucciones.size(); i++) {
+            codigo += this.instrucciones.get(i).get4D(tabla, arbol);
+        }
+        codigo += "jmp,,," + label2 + "\n";
+        codigo += label1 + ":\n";
+        return codigo;
+    }
+}
